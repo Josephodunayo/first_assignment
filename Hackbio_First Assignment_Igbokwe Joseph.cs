687dac30dcf5e959b8d240f40c2663e4bdd291bc
Josephodunayo <69094436+Josephodunayo@users.noreply.github.com>
2020-08-09 15:01:28 -0700

    Add files via upload

---
+++ Hackbio_First Assignment_Igbokwe Joseph.cs
@@ -0,0 +1,41 @@
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text.RegularExpressions;
+
+namespace Rextester
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            //Your code goes here
+
+Console.WriteLine("Name : Igbokwe Joseph \n");
+
+Console.WriteLine("Email :igbokweodunayo@gmail.com \n");
+
+Console.WriteLine("Language : C# \n");
+
+Console.WriteLine("Biostack : Genomics \n");
+
+Console.WriteLine("Username : @Joseph");
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
